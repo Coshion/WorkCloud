ecf98f034833000501b5676b0568bafbcd19cfe5
shishuai <ss_962026658@126.com>
2022-03-11 20:11:15 +0800

    nacos改eureka

---
+++ workcloud-app/src/main/java/com/coshion/workcloud/WorkcloudApplication.java
@@ -0,0 +1,13 @@
+package com.coshion.workcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkcloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkcloudApplication.class, args);
+    }
+
+}
